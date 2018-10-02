68c04c4c78a327c39baee4e17680689c452989d1
Andrew Way <arw405@mun.ca>
2018-10-02 20:41:18 -0230

    Added function which finds the coordinates of a Canny edge

---
+++ image_extraction/find_mip_helpers/calc_edge_positions.m
@@ -0,0 +1,28 @@
+function [ coords ] = calc_edge_positions( bw_img)
+%CALC_EDGE_POSITIONS stores the positions of all white pixels in bw_img
+%In a 2D vector
+n=size(bw_img,1);
+m=size(bw_img,2);
+
+coords=zeros(n*m,2);
+index=1;
+
+for i=1:n
+    for j=1:m
+        if(bw_img(i,j)==1)
+            coords(index,:)=[j,i];
+            index=index+1;
+        end
+    end
+end
+coords=coords(1:index-1,:);
+
+test=zeros(n,m);
+for i=1:index-1
+    x=coords(i,1);
+    y=coords(i,2);
+    test(y,x)=1;
+end
+imshow(test);
+end
+
