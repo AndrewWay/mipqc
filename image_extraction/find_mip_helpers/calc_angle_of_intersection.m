b37adcc09c75c65268cdacee2a2dee1d14f665be
Andrew Way <arw405@mun.ca>
2018-10-02 20:41:44 -0230

    Added functin which calculates angle of intersection between two lines

---
+++ image_extraction/find_mip_helpers/calc_angle_of_intersection.m
@@ -0,0 +1,8 @@
+function [ th ] = calc_angle_of_intersection( line1, line2 )
+%CALC_ANGLE_OF_INTERSECTION Summary of this function goes here
+%   Detailed explanation goes here
+% linex is of the form [theta;rho]
+th=abs(line1(1,1)-line2(1,1));
+
+end
+
