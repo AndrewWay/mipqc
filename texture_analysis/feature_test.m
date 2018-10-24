af7bf69edce4e56dfd7f5dcd2ebc3f8911b35922
Andrew Way <arw405@mun.ca>
2018-10-24 11:32:25 -0230

    Added functions for calculating median and mean gray levels

---
+++ texture_analysis/feature_test.m
@@ -0,0 +1,23 @@
+I=imread('B5.jpg');
+mip=extract_mip(I);
+imshow(mip);
+
+img_dim1Divisions=50;
+img_dim2Divisions=100;
+
+regionCells = dice(mip,img_dim1Divisions,img_dim2Divisions);
+means=zeros(img_dim1Divisions,img_dim2Divisions);
+medians=zeros(img_dim1Divisions,img_dim2Divisions);
+stds=zeros(img_dim1Divisions,img_dim2Divisions);
+skewnesses=zeros(img_dim1Divisions,img_dim2Divisions);
+kurtoses=zeros(img_dim1Divisions,img_dim2Divisions);
+
+for i=1:img_dim1Divisions
+    for j=1:img_dim2Divisions
+        rgbmat=cell2mat(regionCells(i,j));
+        means(i,j)=mean_gray_level(rgbmat);
+    end
+end
+
+histogram(features)
+
---
+++ texture_analysis/feature_vectors/mean_gray_level.m
@@ -0,0 +1,21 @@
+function [ mgl ] = mean_gray_level(I)
+%MEAN_GRAY_LEVEL Summary of this function goes here
+%   Detailed explanation goes here
+size1=size(I,1);
+size2=size(I,2);
+size3=size(I,3);
+
+if(size3>1)
+    I=rgb2gray(I);    
+end
+
+mgl=0;
+for i=1:size1
+    for j=1:size2
+        px_gl=double(I(i,j));
+       mgl=double(mgl+px_gl);
+    end    
+end
+mgl=double(mgl/(size1*size2));
+end
+
