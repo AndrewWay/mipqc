7097ea58b25f5f12131c7b37310934993918a956
Andrew Way <arw405@mun.ca>
2018-11-20 11:56:56 -0330

    Implemented filtering white blocks.

--- image_extraction/find_mip_helpers/filter_white_blocks.m
+++ image_extraction/find_mip_helpers/filter_white_blocks.m
@@ -3,6 +3,7 @@ function [ lines11,lines22 ] = filter_white_blocks( lines11,lines22,...
 %FILTER_WHITE_BLOCKS Summary of this function goes here
 %   Detailed explanation goes here
 
+bw_thresh=0.37;
 %lines11 contain 2 mostly parallel lines chosen for the sides of the
 %rectangle. Similarly for lines22, except they are mostly orthogonal to
 %the lines in lines11.
@@ -72,11 +73,15 @@ for i=1:nLines1
             fprintf("%s\n",'Lines Parallel. New line is within rectangle.');
             %Find the line from lines11 that line1_i is closest to
             if(abs(r_i-lines11(2,1))<abs(r_i-lines11(2,2)))
-                %Replace lines11(:,1) with line1_i
-                lines11(:,2)=[t_i;r_i];
-            else
                 %Replace lines11(:,2) with line1_i
+                replaced_line=lines11(:,2);
                 lines11(:,2)=[t_i;r_i];
+                lines11_2_replaced=1;
+            else
+                %Replace lines11(:,1) with line1_i
+                replaced_line=lines11(:,1);
+                lines11(:,1)=[t_i;r_i];
+                lines11_2_replaced=0;
             end
             
             [leveled_I,leveled_lines11,leveled_lines22]=...
@@ -90,21 +95,16 @@ for i=1:nLines1
             
             %Extract the leveled section
             extracted_mip = extract_rectangle(leveled_I,px1,py1,px2,py2);
-            bw_mip_fragment = im2bw(extracted_mip);
-            
+            bw_mip_fragment = im2bw(extracted_mip,bw_thresh);
+            whiteness=truth(bw_mip_fragment);
             figure(1),imshow(draw_lines(leveled_lines11,...
                 leveled_lines22,leveled_I));
             figure(2),imshow(extracted_mip);
             figure(3),imshow(bw_mip_fragment);
-            fprintf("Truth: %d\n",truth(bw_mip_fragment));
+            fprintf("Truth: %d\n",whiteness);
             pause;
             close all;
-            
-        end
-        
-        
-        
-        %Form the rectangle from that closest line, and the two lines from
+                   %Form the rectangle from that closest line, and the two lines from
         %lines22
         
         %Calculate truth
@@ -112,7 +112,100 @@ for i=1:nLines1
         %If greater than threshold,
         
         %Replace the closest chosen line with line1_i
+         
+            %This is really confusing
+            if(whiteness>=0.9)
+                if(lines11_2_replaced)
+                    lines11(:,2)=replaced_line;
+                    lines11(:,1)=[t_i;r_i];
+                else
+                    lines11(:,1)=replaced_line;
+                    lines11(:,2)=[t_i;r_i];    
+                end
+            else
+                if(lines11_2_replaced)
+                    lines11(:,2)=replaced_line;
+                else
+                    lines11(:,1)=replaced_line;
+                end    
+            end
+        end
+    end
+end
+
+
+for i=1:nLines2
+    line2_i=lines2(:,i);
+    %Find the closest line from lines22
+    t_i=line2_i(1,1);
+    r_i=line2_i(2,1);
+    
+    %check if the current line is inside the rectangle
+    if(lines22_parallel)
+        %Check if current line rho < line11_1 rho and
+        %rho > line11_2 rho
+        if((r_i<lines22(2,1)&&r_i>lines22(2,2))...
+                ||(r_i<lines22(2,2)&&r_i>lines22(2,1)))
+            fprintf("%s\n",'Lines Parallel. New line is within rectangle.');
+            %Find the line from lines11 that line1_i is closest to
+            if(abs(r_i-lines22(2,1))<abs(r_i-lines22(2,2)))
+                %Replace lines11(:,2) with line1_i
+                replaced_line=lines22(:,2);
+                lines22(:,2)=[t_i;r_i];
+                lines22_2_replaced=1;
+            else
+                %Replace lines22(:,1) with line1_i
+                replaced_line=lines22(:,1);
+                lines22(:,1)=[t_i;r_i];
+                lines22_2_replaced=0;
+            end
+            
+            [leveled_I,leveled_lines11,leveled_lines22]=...
+                level_mip(I,lines11,lines22);
+            
+            [xIntersections, yIntersections] = find_intersections(...
+                leveled_lines11,...
+                leveled_lines22);
+            
+            [px1,py1,px2,py2]=find_perfect_vertices(xIntersections,yIntersections);
+            
+            %Extract the leveled section
+            extracted_mip = extract_rectangle(leveled_I,px1,py1,px2,py2);
+            bw_mip_fragment = im2bw(extracted_mip,bw_thresh);
+            whiteness=truth(bw_mip_fragment);
+            figure(1),imshow(draw_lines(leveled_lines11,...
+                leveled_lines22,leveled_I));
+            figure(2),imshow(extracted_mip);
+            figure(3),imshow(bw_mip_fragment);
+            fprintf("Truth: %d\n",whiteness);
+            pause;
+            close all;
+                   %Form the rectangle from that closest line, and the two lines from
+        %lines22
         
+        %Calculate truth
+        
+        %If greater than threshold,
+        
+        %Replace the closest chosen line with line1_i
+         
+            %This is really confusing
+            if(whiteness>=0.9)
+                if(lines22_2_replaced)
+                    lines22(:,2)=replaced_line;
+                    lines22(:,1)=[t_i;r_i];
+                else
+                    lines22(:,1)=replaced_line;
+                    lines22(:,2)=[t_i;r_i];    
+                end
+            else
+                if(lines22_2_replaced)
+                    lines22(:,2)=replaced_line;
+                else
+                    lines22(:,1)=replaced_line;
+                end    
+            end
+        end
     end
 end
 
