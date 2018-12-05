48b64fe2bc62367c5f2d333531958b0ce7d23831
Andrew Way <arw405@mun.ca>
2018-12-05 20:29:55 -0330

    Added script for training SOM

---
+++ texture_analysis/runsom.m
@@ -0,0 +1,62 @@
+%http://www.ai-junkie.com/ann/som/som4.html
+%Each node contains a vector of weights of the same dimension as input vecs
+
+%Initialize SOM
+%Set each vector in the SOM to a vector of random weights
+net = selforgmap([som_dim2 som_dim3]);
+som_dim2 = 20;
+som_dim3 = 20;
+
+inputs=data';
+
+[net, tr] =train(net,inputs);
+
+pngFileName = strcat('data/B/MIP5.jpg');
+if exist(pngFileName, 'file')
+    
+    %Read into MIP image to matrices
+    I = imread(pngFileName);
+        %!!!!DELETE
+%         if(k<=10)
+%             I=I(:,900:3480,:);% DELETE THIS!!
+%         else
+%             I=I(:,1485:4090,:);% DELETE THIS!!
+%         end
+        %!!!!
+    % Find and return the MIP.
+    [MIP]=extract_mip(I);
+        
+    %Use the following line if you don't want to extract
+   % MIP=I;
+    
+    imageDim1 = size(MIP,1);
+    imageDim2 = size(MIP,2);
+    
+    %Dice the matrix into regions
+    uData=createFeatVecs(MIP,nFeats,img_dim1Divisions,img_dim2Divisions);
+    uData=uData*tCoeff;%Transform unknown data 
+    %imgFeatVec=imgFeatVec*tCoeff;
+    
+    index=1;
+    %Find BMU
+    imshow(MIP);
+    hold on;
+    for i=1:img_dim1Divisions
+        for j=1:img_dim2Divisions
+            featVec = uData(index,:)';
+            clusterIndex=vec2ind(net(featVec));
+            
+            fprintf("%3d ",clusterIndex);
+            [x1,x2,y1,y2] = sectionCorners(i,j,imageDim1/img_dim1Divisions,imageDim2/img_dim2Divisions);
+            rectangle('Position',[x1,y1,x2-x1,y2-y1],...
+                'Curvature',[0,0],...
+                'EdgeColor', indexToColor(clusterIndex,som_dim2,som_dim3),...
+                'LineWidth', 1,...
+                'LineStyle','-')
+            index=index+1;
+        end
+        fprintf("\n");
+    end
+
+
+end
