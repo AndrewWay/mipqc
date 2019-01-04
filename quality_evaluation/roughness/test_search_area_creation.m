ae249b39cbe64eb627d35c3d3a3ed02a2ad28484
Andrew Way <arw405@mun.ca>
2019-01-04 10:17:03 -0330

    Issues??

--- quality_evaluation/roughness/test_search_area_creation.m
+++ quality_evaluation/roughness/test_search_area_creation.m
@@ -7,7 +7,7 @@ close all
 min_grade=0.7;
 max_grade=1-min_grade;
 
-I0 = imread('data/B/MIP2.jpg');
+I0 = imread('data/B/MIP3.jpg');
 if size(I0,2)>640
     I0 = imresize(I0, 640/size(I0,2));
     scale_factor=size(I0,2)/640;
@@ -60,7 +60,7 @@ angles(4)=lines22(1,2);
 imshow(I0);
 hold on;
 
-for side=1:4
+for side=2:2
     l=sides(:,:,side);
 q1=l(:,1);
 q2=l(:,2);
@@ -82,13 +82,17 @@ y2=q2(2,1);
 
 ds=a/2;
 
+line_length=sqrt((x2-x1)^2+(y2-y1)^2);
+n=floor(line_length/ds);
+
 if(x1==x2)
-    n=floor((y2-y1)/ds);
-    ds=abs((y2-y1)/n);
     if(y1>y2)
-       tmp=y1;
+       tmpx=x1;
+       tmpy=y1;
+       x1=x2;
        y1=y2;
-       y2=tmp;      
+       x2=tmpx;
+       y2=tmpy;
     end
     m=(x2-x1)/(y2-y1);
 else    
@@ -100,8 +104,6 @@ else
         x1=tmpx;
         y1=tmpy;
     end
-    n=floor((x2-x1)/ds);
-    ds=abs((x2-x1)/n);
     m=(y2-y1)/(x2-x1);
 end
 
@@ -121,6 +123,7 @@ set(line(l(1,:),l(2,:),'linewidth',2,'color',[1,0,0]));
 
 max_bin_err=a;
 
+
 for i=1:n
     qi=[xi;yi];
     [p1,p2]=calc_search_vertices(qi,th,a);
@@ -129,7 +132,7 @@ for i=1:n
         xip1=xi;
         yip1=yi+ds;
     else
-        xip1=xi+ds;
+        xip1=xi+abs(ds*sin(th));
         yip1=m*xip1+b; 
     end
     
@@ -139,9 +142,9 @@ for i=1:n
     bin_err=0;
     n_points_in_bin=0;
     
-    for i=1:size(mip_edge_coords,1)
-        x=mip_edge_coords(i,1);
-        y=mip_edge_coords(i,2);
+    for j=1:size(mip_edge_coords,1)
+        x=mip_edge_coords(j,1);
+        y=mip_edge_coords(j,2);
         
         if(point_is_in_box(x,y,p1,p2,p3,p4))
             n_points_in_bin=n_points_in_bin+1;
