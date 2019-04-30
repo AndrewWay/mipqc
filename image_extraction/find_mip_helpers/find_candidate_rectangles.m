65f5cbe543f636ab6e821b1d301a76193aefb5fc
AndrewWay <arw405@mun.ca>
2019-04-30 09:38:55 -0230

    Re-added script specifically for finding MIP-like textures

--- image_extraction/find_mip_helpers/find_candidate_rectangles.m
+++ image_extraction/find_mip_helpers/find_candidate_rectangles.m
@@ -1,10 +1,38 @@
-function [outputArg1,outputArg2] = find_candidate_rectangles(lines11_lines22)
+function [outputArg1,outputArg2] = find_candidate_rectangles(lines11_lines22,scale_factor)
 %FIND_CANDIDATE_RECTANGLES Summary of this function goes here
 %   Detailed explanation goes here
 max_number_of_candidates = 4;
-candidates=[];
-
+nLines1=size(lines11,2);
+nLines2=size(lines22,2);
 %grade lines
+candidates = [];
+
+for i=1:nLines1
+    linei=lines11(:,i);
+    ti=lines11(1,i);
+    ri=lines11(2,i);
+    for j=i+1:nLines1
+        linej=lines11(:,j);
+        tj=lines11(1,j);
+        rj=lines11(2,j);
+        for k=1:nLines2
+            linek=lines22(:,k);
+            tk=lines22(1,k);
+            rk=lines22(2,k);
+            for l=k+1:nLines2
+                linel=lines22(:,l);
+                tl=lines22(1,l);
+                rl=lines22(2,l);
+                
+                [tmp_fit_grade]=grade_rectangle([linei,linej],[linel,linek],scale_factor);
+                Itmp=I;
+                candidates=[candidates;i,j,k,l,tmp_fit_grade];
+
+                
+            end
+        end
+    end
+end
 
 end
 
