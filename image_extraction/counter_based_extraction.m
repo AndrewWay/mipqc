f94ee69f6b53f075e4d1bf547d25f4ef0126b47c
AndrewWay <arw405@mun.ca>
2019-05-07 16:50:24 -0230

    Saving

---
+++ image_extraction/counter_based_extraction.m
@@ -0,0 +1,6 @@
+function [outputArg1,outputArg2] = counter_based_extraction(texture_errors,lines11,lines22)
+%COUNTER_BASED_EXTRACTION Summary of this function goes here
+%   Detailed explanation goes here
+
+end
+
