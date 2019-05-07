019701049e590d59cd25888d7a5a5ceb177e07d6
AndrewWay <arw405@mun.ca>
2019-05-07 19:16:27 -0230

    Implemented code for creating "MIP Error Image"

---
+++ image_extraction/remove_ruler.m
@@ -0,0 +1,6 @@
+function [I] = remove_ruler(I)
+%REMOVE_RULER Summary of this function goes here
+%   Detailed explanation goes here
+
+end
+
--- image_extraction/texture_based_extraction.m
+++ image_extraction/texture_based_extraction.m
@@ -1,8 +1,7 @@
 %Determines what cells are "mip-like"
 
 
-number_of_image_divs1 = 80;
-number_of_image_divs2 = 80;
+
 
 close all;
 %pngFileName = strcat('data/D/D6943.png');
@@ -22,57 +21,7 @@ if exist(pngFileName, 'file')
     close all;
     %Use the following line if you don't want to extract
     
-    imageDim1 = size(MIP,1);
-    imageDim2 = size(MIP,2);
-    
-    fprintf("Creating uData...\n");
-    
-    index=1;
-    new_cells = dice(MIP,number_of_image_divs1,number_of_image_divs2);
-    neuron_matrix = cell2mat(net.IW);
-    %Find BMU
-    imshow(MIP);
-    hold on;
-    distances=zeros(number_of_image_divs1,number_of_image_divs2);
-    y1=1;
-    for i=1:number_of_image_divs1
-        x1=1;
-        for j=1:number_of_image_divs2
-            img_ij=cell2mat(new_cells(i,j));
-            
-            dim1_ij=size(img_ij,1);
-            dim2_ij=size(img_ij,2);
-            
-            
-            featVec = create_feature_vector(img_ij,nFeatures);
-            %MUST ZSCORE THIS DATA TO MATCH ZDATA (refer to loadData.m)
-            featVec=(featVec-zmu)./zsigma;
-            featVec = featVec*tCoeff;
-            clusterIndex=vec2ind(net(featVec'));
-            %             if(i==38&&j==1)
-            %                figure(3),imshow(img_ij);
-            %                disp(clusterIndex);
-            %                pause;
-            %             end
-            %clusterIndex=findBMU(featVec',cell2mat(net.IW));
-            [in,jn]=ind2sub([som_dim1,som_dim2],clusterIndex);
-            bmu_distance = norm(neuron_matrix(clusterIndex)-featVec,2);
-            distances(i,j)=bmu_distance;
-            
-            if(bmu_distance>4)
-                rectangle('Position',[x1,y1,dim2_ij,dim1_ij],...
-                    'Curvature',[0,0],...
-                    'EdgeColor', [1,0,0],...
-                    'LineWidth', 1,...
-                    'LineStyle','-')
-            end
-            
-            x1=x1+dim2_ij;
-            index=index+1;
-        end
-        y1=y1+dim1_ij;
-        fprintf("\n");
-    end
+   %TODO craete mip imge
     
 contour(distances);
 pause;
